de1c606c91a3a2b4b7dabe7552c07ce690408e64
Dongmin Lee <admin@ldmsys.net>
2023-04-23 23:34:53 +0900

    Revert "fix: per request"
    This reverts commit ee2659945627e2caae0bddb2e847d9db58d9bde2.

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
 #!/usr/bin/env node
-
-const mcproto = require("minecraft-protocol");
-
-let motd = "&b&lTest MC Server&r &k12345";
-let kickMessage = "You are not white-listed on this server!";
-
-(async () => {
-  motd = motd.replace(/&/g, "§");
-  kickMessage = kickMessage.replace(/&/g, "§");
-
-  mcproto.createServer({
+require('minecraft-protocol').createServer({
     'online-mode': false,
     encryption: true,
     host: '0.0.0.0',
     port: 25565,
-    motd: motd,
+    motd: "Test MC Server",
+    version: "1.12.2",
     maxPlayers: 99999,
     playersCount: 99999
-  }).on('login', client => {
-    client.end(kickMessage);
+}).on('login', client => {
+  client.write('login', {
+    entityId: client.id,
+    levelType: 'default',
+    gameMode: 0,
+    dimension: 0,
+    difficulty: 0,
+    reducedDebugInfo: true
   });
-  console.log("[Test] Server is running on 0.0.0.0:25565");
-})();
+    client.write('position', {
+        x: 0,
+        y: 1.62,
+        z: 0,
+        yaw: 0,
+        pitch: 0,
+        flags: 0x00    
+    });
+    client.write("chat", { message: JSON.stringify({translate: "chat.type.announcement", with: [ 'Test', "It works!"]}), position: 0});
+});
+console.log("[Test] Server is running on 0.0.0.0:25565");
